ff143985f6e3ad046b805f87dbd3ef30c4d261bd
marlon4dashen <alienlql@hotmail.com>
2021-03-03 23:29:45 -0500

    add router

---
+++ src/components/LogIn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="signup">
+      <h1> log in class</h1>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'LogIn',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcomecontainer">
+      <h1> welcome class</h1>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Welcome',
+
+}
+</script>
+
+<style>
+
+</style>
